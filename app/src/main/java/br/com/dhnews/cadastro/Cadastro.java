d27d14b51604bd928fbe333e54f20093bdd64a9c
Vinicius Oliveira <vinicius.developer@outlook.com>
2019-05-27 20:45:32 -0300

    Trocando acitivy por fragment e adicionando a base de layout do cadastro

--- app/src/main/java/br/com/dhnews/cadastro/Cadastro.java
+++ app/src/main/java/br/com/dhnews/cadastro/Cadastro.java
@@ -1,15 +1,30 @@
 package br.com.dhnews.cadastro;
 
-import android.support.v7.app.AppCompatActivity;
+
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 import br.com.dhnews.R;
 
-public class Cadastro extends AppCompatActivity {
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class Cadastro extends Fragment {
+
+
+    public Cadastro() {
+        // Required empty public constructor
+    }
+
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_cadastro);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_cadastro, container, false);
     }
+
 }
