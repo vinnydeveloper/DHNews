8fac546cdbbe123d60686657e24a45f5e04d8b09
taddeus10 <leandro.taddeu@gmail.com>
2019-06-10 20:14:15 -0300

    Criação da tela pesquisa

--- app/src/main/java/br/com/dhnews/MainActivy.java
+++ app/src/main/java/br/com/dhnews/MainActivy.java
@@ -11,15 +11,29 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.SearchView;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import br.com.dhnews.Pesquisa.Pesquisa;
 import br.com.dhnews.cadastro.Cadastro;
 import br.com.dhnews.home.Home;
 import br.com.dhnews.login.Login;
 
+import static br.com.dhnews.R.id.navigation;
+
 public class MainActivy extends AppCompatActivity {
+
+
+
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -29,10 +43,12 @@ public class MainActivy extends AppCompatActivity {
             switch (item.getItemId()) {
                 case R.id.navigation_user:
                     replaceFragment(new Login());
-                    return true;
                 case R.id.navigation_home:
                     replaceFragment( new Home());
                     return true;
+                case R.id.navigation_search:
+                    replaceFragment( new Pesquisa());
+                   return true;
             }
             return false;
         }
@@ -48,7 +64,6 @@ public class MainActivy extends AppCompatActivity {
         replaceFragment(new Home());
 
 
-
     }
     public void replaceFragment(Fragment fragment){
         FragmentManager manager = getSupportFragmentManager();
@@ -56,5 +71,6 @@ public class MainActivy extends AppCompatActivity {
         transaction.replace(R.id.container, fragment);
         transaction.commit();
     }
+
 
 }
