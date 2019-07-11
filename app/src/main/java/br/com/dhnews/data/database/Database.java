5785172c9d6ce65ec75ad8fcf6078928340039f4
eduardosiqueira2019 <siqueira007@yahoo.com.br>
2019-07-10 22:25:10 -0300

    Sprint 4: - Implementação inicial: classe DataBase, POJOS, DAO e nova estrutura de pastas

---
+++ app/src/main/java/br/com/dhnews/data/database/Database.java
@@ -0,0 +1,36 @@
+package br.com.dhnews.data.database;
+
+import android.arch.persistence.room.Room;
+import android.arch.persistence.room.RoomDatabase;
+import android.arch.persistence.room.TypeConverters;
+import android.content.Context;
+
+import br.com.dhnews.data.database.dao.NoticiasDAO;
+import br.com.dhnews.data.database.dao.UsuariosDAO;
+import br.com.dhnews.model.Noticias;
+import br.com.dhnews.model.Usuario;
+
+@android.arch.persistence.room.Database(entities = {Noticias.class,Usuario.class}, version = 1, exportSchema = false)
+@TypeConverters(Converters.class)
+public abstract class Database extends RoomDatabase {
+    private static volatile Database INSTANCE;
+
+    public abstract NoticiasDAO noticiasDAO();
+
+    public abstract UsuariosDAO usuariosDAO();
+
+    public static Database getDatabase(Context context) {
+        if (INSTANCE == null) {
+            synchronized (Database.class) {
+                if (INSTANCE == null) {
+                    INSTANCE = Room.databaseBuilder(context, Database.class, "dh_news")
+                            .fallbackToDestructiveMigration()
+                            .build();
+                }
+            }
+        }
+        return INSTANCE;
+    }
+
+
+}
