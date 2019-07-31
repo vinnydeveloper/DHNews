902acedf91c9865cfc54cd604a36ec831872f3f9
mayarals <limas.mayara@gmail,com>
2019-07-31 00:18:42 -0300

    .

--- app/src/main/java/br/com/dhnews/model/noticias/Article.java
+++ app/src/main/java/br/com/dhnews/model/noticias/Article.java
@@ -4,13 +4,10 @@ package br.com.dhnews.model.noticias;
 import android.os.Parcel;
 import android.os.Parcelable;
 
-import androidx.room.Entity;
-
 import com.google.gson.annotations.Expose;
 
 
-@Entity(tableName = "Article")
-public class Article {
+public class Article implements Parcelable {
 
     @Expose
     private Object author;
@@ -38,6 +35,17 @@ public class Article {
         urlToImage = in.readString();
     }
 
+    public static final Creator<Article> CREATOR = new Creator<Article>() {
+        @Override
+        public Article createFromParcel(Parcel in) {
+            return new Article(in);
+        }
+
+        @Override
+        public Article[] newArray(int size) {
+            return new Article[size];
+        }
+    };
 
     public Object getAuthor() {
         return author;
@@ -103,5 +111,18 @@ public class Article {
         this.urlToImage = urlToImage;
     }
 
+    @Override
+    public int describeContents() {
+        return 0;
+    }
 
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(content);
+        dest.writeString(description);
+        dest.writeString(publishedAt);
+        dest.writeString(title);
+        dest.writeString(url);
+        dest.writeString(urlToImage);
+    }
 }
